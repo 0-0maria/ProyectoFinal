2152544beafa9f39b2f73113d9cd2ddee6149c7b
unknown <Sena@MEDAPRCSGFSD792.FormacionSena.RED>
2026-09-24 10:47:16 -0500

    Actualizacion

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 import os
 import psycopg
-from psycopg.extras import RealDictCursor
+from psycopg.rows import dict_row
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,40 +8,36 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
-    return psycopg.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+    return psycopg.connect(DATABASE_URL, row_factory=dict_row)
 
 def crear_tablas():
-    conn = get_connection()
-    cur = conn.cursor()
-    
-    
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS usuarios(
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(50) NOT NULL UNIQUE,
-        password_hash VARCHAR(255) NOT NULL
-    )
-    """)
-    
-    
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS servicios(
-        id SERIAL PRIMARY KEY,
-        nombre VARCHAR(100) NOT NULL,
-        precio NUMERIC(10, 2) NOT NULL
-    )
-    """)
-    
-    
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS cliente_servicios(
-        id SERIAL PRIMARY KEY,
-        idservicio INTEGER REFERENCES servicios(id) ON DELETE CASCADE,
-        fecha DATE NOT NULL,
-        cliente VARCHAR(100) NOT NULL
-    )
-    """)
-    
-    conn.commit()
-    cur.close()
-    conn.close()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS usuarios(
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(50) NOT NULL UNIQUE,
+                password_hash VARCHAR(255) NOT NULL
+            )
+            """)
+            
+            
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS servicios(
+                id SERIAL PRIMARY KEY,
+                nombre VARCHAR(100) NOT NULL,
+                precio NUMERIC(10, 2) NOT NULL
+            )
+            """)
+            
+            
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS cliente_servicios(
+                id SERIAL PRIMARY KEY,
+                idservicio INTEGER REFERENCES servicios(id) ON DELETE CASCADE,
+                fecha DATE NOT NULL,
+                cliente VARCHAR(100) NOT NULL
+            )
+            """)
+        conn.commit()
